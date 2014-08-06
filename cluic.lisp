9a6b5d0b034b5871659dc286678f9bc48fbe5fc2
Aaron France <aaron.l.france@gmail.com>
2014-08-06 21:10:40 +0200

    Fleshed out from-file a bit more

--- cluic.lisp
+++ cluic.lisp
@@ -5,8 +5,10 @@
 
 
 (defun from-file (file)
-  (let ((parsed (parse-file file (cxml-dom:make-dom-builder))))
-    parsed))
+  (let* ((parsed (parse-file file (cxml-dom:make-dom-builder)))
+         (child-nodes (dom:child-nodes parsed)))
+    (assert (= (length child-nodes) 1)))
+  (error "not-implemented"))
 
 (defun from-string (str)
   )
